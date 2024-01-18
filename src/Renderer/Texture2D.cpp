d0bed356e33ce8a48be9d8f7995bd48c920d2ae7
LORAN <vinana222@gmail.com>
2024-01-18 14:32:18 +0300

    Added Texture2D(v1)

---
+++ src/Renderer/Texture2D.cpp
@@ -0,0 +1,33 @@
+#include "Texture2D.h"
+
+namespace Renderer {
+
+	Texture2D::Texture2D(const GLuint width, GLuint height,
+		const unsigned char* data,
+		const unsigned int channels = 4,
+		const GLenum filter = GL_LINEAR,
+		const GLenum wrapMode = GL_CLAMP_TO_EDGE)
+		: m_width(width)
+		, m_height(height)
+	{
+		switch (channels)
+		{
+		case 4:
+			m_mode = GL_RGBA;
+		case 3:
+			m_mode - GL_RGB;
+		default:
+			m_mode = GL_RGBA;
+			break;
+		}
+
+		glGenTextures(1, &m_ID);
+		glBindTexture(GL_TEXTURE_2D, m_ID);
+		glTexImage2D(GL_TEXTURE_2D, 0, m_mode, m_width, m_height, 0, m_mode, GL_UNSIGNED_BYTE, data);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
+	}
+}
