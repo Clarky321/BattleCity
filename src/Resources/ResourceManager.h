cdedb269760f677e1c9c4faa79facd2d49651061
LORAN <vinana222@gmail.com>
2024-01-19 11:59:24 +0300

    Added Texture2D(v2)

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -7,14 +7,13 @@
 namespace Renderer
 {
 	class ShaderProgram;
+    class Texture2D;
 }
 
 class ResourceManager {
-
 public:
     ResourceManager(const std::string& executablePath);
     ~ResourceManager() = default;
-
     ResourceManager(const ResourceManager&) = delete;
     ResourceManager& operator=(const ResourceManager&) = delete;
     ResourceManager& operator=(ResourceManager&&) = delete;
@@ -22,13 +21,17 @@ public:
 
     std::shared_ptr<Renderer::ShaderProgram> loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath);
     std::shared_ptr<Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
-    void loadTexture(const std::string& textureName, const std::string& texturePath);
+    std::shared_ptr<Renderer::Texture2D> loadTexture(const std::string& textureName, const std::string& texturePath);
+    std::shared_ptr<Renderer::Texture2D> getTexture(const std::string& textureName);
 
 private:
     std::string getFileString(const std::string& relativeFilePath) const;
 
     typedef std::map<const std::string, std::shared_ptr<Renderer::ShaderProgram>> ShaderProgramsMap;
     ShaderProgramsMap m_shaderPrograms;
+
+    typedef std::map<const std::string, std::shared_ptr<Renderer::Texture2D>> TexturesMap;
+    TexturesMap m_textures;
 
     std::string m_path;
 };
